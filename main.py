7db84aa24362b005f0158531b7ec4182f87fab89
Riley Druken <rileydruken@gmail.com>
2024-11-17 17:04:47 -0330

    update deck init and display title

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import random
 
 def deck_initialize():
+    # Creates the deck of cards and shuffles them
     suits = ["Hearts","Clubs","Diamonds","Spades"]
-    values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+    ranks = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"]
+    values = [[1,11],2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+
     deck = []
 
-    for i in suits:
-        for x in values:
-            deck.append([i,x])
+    for suit in suits:
+        for i in range(len(ranks)):
+            deck.append([suit,ranks[i],values[i]])
 
     random.shuffle(deck)
-
     return deck
 
 
+
+
 def main():
+    print("BlACKJACK!\nBlackjack payout is 3:2\n")
     deck = deck_initialize()
     print(deck)
+
+
+
+
+
 
 
 if __name__ == '__main__':
