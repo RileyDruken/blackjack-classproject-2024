3713af3472804188248b87fe80e52720ef4d7192
Riley Druken <rileydruken@gmail.com>
2024-11-17 17:11:48 -0330

    Add game initialization

--- main.py
+++ main.py
@@ -16,12 +16,30 @@ def deck_initialize():
     return deck
 
 
+def initialize_game(dealer,player,deck):
+    # initializes the game by dealer the cards to the player and dealer
+    for i in range(2):
+        card = random.choice(deck)
+        dealer.append(card)
+        deck.remove(card)
+
+        card = random.choice(deck)
+        player.append(card)
+        deck.remove(card)
+
 
 
 def main():
     print("BlACKJACK!\nBlackjack payout is 3:2\n")
     deck = deck_initialize()
     print(deck)
+
+    dealer = []
+    player = []
+
+
+
+
 
 
 
