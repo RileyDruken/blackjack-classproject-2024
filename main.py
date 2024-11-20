f7ec0a9786d1ace91890aa097a27ea4a86f76acb
Riley Druken <rileydruken@gmail.com>
2024-11-20 00:05:37 -0330

    cleaned up code - added play again and place bet function - removed ace function to reimplement

--- main.py
+++ main.py
@@ -15,37 +15,37 @@ def deck_initialize():
     random.shuffle(deck)
     return deck
 
+def initialize_game(deck):
+    dealer = []
+    player = []
 
-def initialize_game(dealer, player, deck, money):
     # Initializes the game by dealing the cards to the player and dealer
     for i in range(2):
         card = deck.pop()
-        ace_check(card, 0, 0, True)
         dealer.append(card)
 
         card = deck.pop()
-        ace_check(card,0,0,False)
         player.append(card)
-
-    deck = deck_initialize()
-    money = float(db.read_money())
-
-
     print("\nDEALER's SHOW CARD:")
     print(f"{dealer[0][1]} of {dealer[0][0]}\n")
-    return money
 
-def calculate_scores(dealer,player):
+    return  dealer, player
+
+def calculate_score_player(player):
     #calculates scores
-    dealer_score = 0
     player_score = 0
+
+    for i in player:
+        player_score += i[2]
+    return player_score
+
+def calculate_score_dealer(dealer):
+    #calculates score
+    dealer_score = 0
 
     for i in dealer:
         dealer_score += i[2]
-    for i in player:
-        player_score += i[2]
-
-    return dealer_score,player_score
+    return dealer_score
 
 def display_player_cards(player):
     # displays player cards
@@ -63,8 +63,11 @@ def display_dealer_cards(dealer):
 
 def win_check(dealer_score, player_score,bet_amount,money):
     # checks if the winner
-    print(f"YOUR POINTS:\t {player_score}")
-    print(f"DEALER'S POINTS: {dealer_score}")
+    if player_score < 21:
+        print(f"YOUR POINTS:\t {player_score}")
+        print(f"DEALER'S POINTS: {dealer_score}\n")
+    else:
+        print(f"YOUR POINTS:\t {player_score}")
 
     if player_score == 21:
         print("Congrats, You Win.")
@@ -88,134 +91,82 @@ def win_check(dealer_score, player_score,bet_amount,money):
     print("Money: ", money)
     db.write_money(str(money))
 
-def ace_check(card, player_score, dealer_score, is_dealer=False):
-    #checks if the card the player or dealer has is an ace
-    if card[1] == "Ace":
-        if is_dealer:
-            if dealer_score + 11 > 21:
-                card[2] = 1
-            else:
-                card[2] = 11
-        else:
-            if player_score + 11 > 21:
-                card[2] = 1
-            else:
-                while True:
-                    choice = input("Would you like your Ace to be worth 1? By default, it will be valued at 11. (y/n): ").lower()
-                    print()
-                    if choice == "y":
-                        card[2] = 1
-                        break
-                    elif choice == "n":
-                        card[2] = 11
-                        break
-                    else:
-                        print("Invalid choice. Please try again.")
 
-def dealer_turn(dealer, deck, dealer_score, player):
-    # makes the dealer play their turn
+def dealer_turn(dealer, deck):
+    dealer_score = 0
+
+    # Draw cards until the dealer's score reaches 17 or higher
     while dealer_score < 17:
         card = deck.pop()
-        ace_check(card,0,dealer_score,True)
-
         dealer.append(card)
-        dealer_score, player_score = calculate_scores(dealer, player)
+        # Calculate the dealer's score after drawing each card
+        for i in dealer:
+            dealer_score += i[2]
     return dealer_score
 
-def main():
-    dealer = []
-    player = []
-
+def place_bet():
     money = float(db.read_money())
-    print("BlACKJACK!\nBlackjack payout is 3:2\n")
-
-    deck = deck_initialize()
     print(f"Money: {money}")
-
     while True:
         try:
             bet_amount = float(input("Bet amount: "))
-            if bet_amount >= 5 and bet_amount <= 1000 and bet_amount <= money:
+            if 5 <= bet_amount <= money and bet_amount <= 1000:
                 break
             else:
                 print("Invalid bet amount")
         except ValueError:
             print("Invalid input for bet amount try again.")
-    initialize_game(dealer, player, deck, money)
+    return bet_amount, money
 
+def play_again():
+    while True:
+        player_choice = input("Play again? (y/n): ").lower()
+        if player_choice == "y" or player_choice == "n":
+            break
+        print("Invalid input! please try again")
+    if player_choice == "y":
+        return False
+    else:
+        return True
 
-    display_player_cards(player)
+def main():
+    print("BlACKJACK!\nBlackjack payout is 3:2\n")
 
     while True:
+        deck = deck_initialize()
+        bet_amount, money = place_bet()
+        dealer, player = initialize_game(deck)
+        display_player_cards(player)
 
-        dealer_score, player_score = calculate_scores(dealer, player)
+        while True:
+            player_score = calculate_score_player(player)
+            if player_score > 21:
+                # dealer score 0 cause dealer score is not needed when player bust since they lose any ways dealer turn is skipped
+                display_dealer_cards(dealer)
+                display_player_cards(player)
 
-        if player_score > 21:
-            win_check(dealer_score,player_score,bet_amount,money)
-            while True:
-                player_choice = input("Play again? (y/n): ").lower()
-                if player_choice == "y" or player_choice == "n":
-                    break
-                print("Invalid input! please try again")
-            if player_choice != "y":
+                win_check(0,player_score,bet_amount,money)
                 break
-            print()
-            # resets values for new game
-            dealer = []
-            player = []
-            initialize_game(dealer, player, deck,money)
-            display_player_cards(player)
 
-        choice = input("Hit or stand? (hit/stand): ").lower()
+            choice = input("Hit or stand? (hit/stand): ").lower()
+            if choice == "hit":
+                card = deck.pop()
+                player.append(card)
+                display_player_cards(player)
 
-        if choice == "hit":
-            card = deck.pop()
-            ace_check(card,player_score,dealer_score,False)
+            elif choice == "stand":
 
-            player.append(card)
-            display_player_cards(player)
+                print()
 
-        elif choice == "stand":
-            dealer_score = dealer_turn(dealer, deck, dealer_score, player)
-
-            print()
-            display_dealer_cards(dealer)
-
-            #check if winner
-            win_check(dealer_score, player_score,bet_amount,money)
-
-            #sees if the player wants to end the game
-            while True:
-                player_choice = input("Play again? (y/n): ").lower()
-                if player_choice == "y" or player_choice == "n":
-                    break
-                print("Invalid input! please try again")
-            if player_choice != "y":
+                dealer_score = dealer_turn(dealer, deck)
+                display_dealer_cards(dealer)
+                win_check(dealer_score, player_score, bet_amount, money)
                 break
-            print()
 
-            money = float(db.read_money())
-            print("Money:", money)
-            while True:
-                try:
-                    bet_amount = float(input("Bet amount: "))
-                    if bet_amount >= 5 and bet_amount <= 1000 and bet_amount <= money:
-                        break
-                    else:
-                        print("Invalid bet amount")
-                except ValueError:
-                    print("Invalid input for bet amount try again.")
-            # resets values for new game
-            dealer = []
-            player = []
-            initialize_game(dealer, player, deck, money)
-
-            display_player_cards(player)
-
-        else:
-            print("invalid choice please try again!")
-
-
+            else:
+                print("invalid choice please try again!")
+        if play_again():
+            break
     print("\nCome back soon!\nBye!")
 
 
